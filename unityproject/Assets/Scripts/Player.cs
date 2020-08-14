ffddba0315ad955fb41a531dab7fc11837ab12db
Santiago Swinnen <sswinnen@itba.edu.ar>
2020-08-13 21:21:23 -0300

    Player movement

---
+++ unityproject/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movSpeed = 200f;
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            var xpos = transform.position.x - movSpeed * Time.deltaTime;
+            transform.position = new Vector3(xpos, transform.position.y, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            var xpos = transform.position.x + movSpeed * Time.deltaTime;
+            transform.position = new Vector3(xpos, transform.position.y, 0);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            var ypos = transform.position.y + movSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, ypos, 0);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            var ypos = transform.position.y - movSpeed * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, ypos, 0);
+        }
+    }
+}
