8ea23fa4a19b5f8234a188525a4a3f7cd4127395
mlund <mlund@itba.edu.ar>
2020-08-23 18:49:59 -0300

    Add camera manager

---
+++ unityproject/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float horizontalResolution = 1920;
+ 
+    void OnGUI ()
+    {
+        switch (Screen.width)
+        {
+            case 200:
+                Camera.main.orthographicSize = 144;
+                break;
+            case 480:
+                Camera.main.orthographicSize = 120;
+                break;
+        }
+    }
+}
